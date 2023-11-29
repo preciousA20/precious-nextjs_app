1435096019b45400d15bde9a707435a1dbbd08fd
ushiepius <preciousprogrammer2@gmail.com>
2023-11-29 17:58:57 +0100

    my nextjs project

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function loginPage(){
+    return(
+        <div className="bg-slate-600">
+            <h2 className="bg-yellow-400 text-center">Login page</h2>
+        </div>
+    )
+}
---
+++ app/signUp/page.tsx
@@ -0,0 +1,7 @@
+export default function signUp(){
+    return(
+        <div className="bg-slate-600">
+            <h2 className="bg-green-700 text-center">Sign up page</h2>
+        </div>
+    )
+}
